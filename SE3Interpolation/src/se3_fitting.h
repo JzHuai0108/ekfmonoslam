fa6604014cc2552a720616f5b2f364a7765cd590
Jianzhu Huai <huai.3@osu.edu>
2018-08-07 16:57:10 +0800

    update indices

--- SE3Interpolation/src/se3_fitting.h
+++ SE3Interpolation/src/se3_fitting.h
@@ -8,55 +8,62 @@
 
 #include "sophus/se3.hpp"
 
-// interpolate IMU data given poses and their near uniform timestamps
-// input: q02n and times of the same length: q_0^w at t_0, q_1^w at t_1,
-// ..., q_n^w at t_n
-// N=n+1 sensor to world transforms and their timestamps; also outputFreq
-
-// output: samplePoses and samples are sampled sensor to world transforms and
-// inertial samples. Each sample contains timestamps, accel of the sensor
-// by the combined force (applied force and gravity) in the world frame,
-// and angular rate of the sensor w.r.t the world frame represented in 
-// the sensor frame, and velocity of the sensor in the world frame
-// output timestamps are times[0], times[0]+1/outputFreq, 
-// times[0]+2/outputFreq, ...
-
+/**
+ *@brief interpolate IMU data given control poses and their uniform timestamps
+ * @param q02n, nominal trajecotry poses,i.e., control points, q_0^w, q_1^w, ..., q_n^w; 
+ * N=n+1 sensor to world transforms
+ * for interpolation, one pose is added at both ends of the array of q02n, making its size n+3.
+ * The two poses are added assuming constant velocity at the start and the end.
+ * @param times, N timestamps, assume equally spaced in time
+ * @param outputFreq, output frequency of true inertial data
+ * @param samplePoses, output sampled sensor to world transforms
+ * @param samples, output inertial samples. Each entry: timestamps, acceleration of sensor
+ * by the combined force (specific forces and gravity) in the world frame if gw is not specified,
+ * otherwise, acceleration of sensor by the specific force in the sensor frame,
+ * and angular rate of sensor w.r.t world frame represented in the sensor frame, 
+ * and velocity of sensor in world frame
+ * output timestamps are times[0], times[0]+1/outputFreq, times[0]+2/outputFreq, ...
+*/
 template<class Scalar>
 void InterpolateIMUData(const std::vector<Sophus::SE3Group<Scalar>, Eigen::aligned_allocator<Sophus::SE3Group<Scalar> > > &q02n,
                         const std::vector<Scalar>& times,
                         const Scalar outputFreq,
                         std::vector<Eigen::Matrix<Scalar, 4, 4>, Eigen::aligned_allocator<Eigen::Matrix<Scalar, 4, 4> > >& samplePoses,
-                        std::vector<Eigen::Matrix<Scalar, 10, 1>, Eigen::aligned_allocator<Eigen::Matrix<Scalar, 10, 1> > >& samples)
+                        std::vector<Eigen::Matrix<Scalar, 10, 1>, Eigen::aligned_allocator<Eigen::Matrix<Scalar, 10, 1> > >& samples, 
+                        const Eigen::Matrix<Scalar, 3, 1> gw = Eigen::Matrix<Scalar, 3, 1>::Zero())
 {
+    bool outputaccelinw = true;
+    if (!gw.norm() == 0)
+      outputaccelinw = false;
     typedef Sophus::SO3Group<Scalar> SO3Type;
     typedef Sophus::SE3Group<Scalar> SE3Type;
     typedef typename Sophus::SE3Group<Scalar>::Tangent Tangent;
 
     std::cout<<"Assigning control points"<<std::endl;
     int lineNum=q02n.size();
-    std::vector<SE3Type, Eigen::aligned_allocator<SE3Type> > bm32nm1(lineNum+2); // b_-3^w, b_-2^w, ..., b_(n-1)^w
-    std::vector<Tangent, Eigen::aligned_allocator<Tangent> > Omegam22nm1(lineNum+1); // $\Omega_-2, \Omega_-1, ..., \Omega_n-1$ where $\Omega_j=log((b_j-1)\b_j)$
-// assume initial velocity is zero
-//    Omegam22nm1[1]=SE3Type::log(q02n[0].inverse()*q02n[1]); //\Omega_-1
-//    Omegam22nm1[0]=SE3Type::vee(-SE3Type::exp(Omegam22nm1[1]/6).matrix()*SE3Type::hat(Omegam22nm1[1])*
-//            SE3Type::exp(-Omegam22nm1[1]/6).matrix());
-//    bm32nm1[1]=q02n[0];
-//    bm32nm1[2]=q02n[1];
-//    bm32nm1[0]=bm32nm1[1]*SE3Type::exp(-Omegam22nm1[0]);
+    std::vector<SE3Type, Eigen::aligned_allocator<SE3Type> > bm12np1(lineNum+2); // b_-1^w, b_0^w, ..., b_(n+1)^w
+    std::vector<Tangent, Eigen::aligned_allocator<Tangent> > Omega02np1(lineNum+1); // $\Omega_0, \Omega_1, ..., \Omega_n+1$ where $\Omega_j=log((b_j-1)\b_j)$
+// assume initial velocity is zero, often cause jumps in acceleration, not recommended
+//    Omega02np1[1]=SE3Type::log(q02n[0].inverse()*q02n[1]); //\Omega_1
+//    Omega02np1[0]=SE3Type::vee(-SE3Type::exp(Omega02np1[1]/6).matrix()*SE3Type::hat(Omega02np1[1])*
+//            SE3Type::exp(-Omega02np1[1]/6).matrix());
+//    bm12np1[1]=q02n[0];
+//    bm12np1[2]=q02n[1];
+//    bm12np1[0]=bm12np1[1]*SE3Type::exp(-Omega02np1[0]);
 // or assume first three poses have identical difference
-    bm32nm1[1]=q02n[0];
-    bm32nm1[2]=q02n[1];
-    bm32nm1[0]=bm32nm1[1]*bm32nm1[2].inverse()*bm32nm1[1];
-    Omegam22nm1[0]=SE3Type::log(bm32nm1[0].inverse()*bm32nm1[1]);
-    Omegam22nm1[1]=SE3Type::log(q02n[0].inverse()*q02n[1]); //\Omega_-1
+    bm12np1[1]=q02n[0];
+    bm12np1[2]=q02n[1];
+    bm12np1[0]=bm12np1[1]*bm12np1[2].inverse()*bm12np1[1];
+    Omega02np1[0]=SE3Type::log(bm12np1[0].inverse()*bm12np1[1]);
+    Omega02np1[1]=SE3Type::log(q02n[0].inverse()*q02n[1]); //\Omega_1
 
     for (int i=3; i<lineNum+1; ++i)
     {
-        bm32nm1[i]=q02n[i-1];
-        Omegam22nm1[i-1]=SE3Type::log(bm32nm1[i-1].inverse()*bm32nm1[i]);
+        bm12np1[i]=q02n[i-1];
+        Omega02np1[i-1]=SE3Type::log(bm12np1[i-1].inverse()*bm12np1[i]);
     }
-    bm32nm1[lineNum+1]=q02n[lineNum-1]*q02n[lineNum-2].inverse()*q02n[lineNum-1];
-    Omegam22nm1[lineNum]=SE3Type::log(bm32nm1[lineNum].inverse()*bm32nm1[lineNum+1]);
+    bm12np1[lineNum+1]=q02n[lineNum-1]*q02n[lineNum-2].inverse()*q02n[lineNum-1];
+    Omega02np1[lineNum]=SE3Type::log(bm12np1[lineNum].inverse()*bm12np1[lineNum+1]);
 
     std::cout<<"Take derivatives to compute acceleration and angular rate"<<std::endl;
     int dataCount=floor((*(times.rbegin())-1e-6-times[0])*outputFreq)+1; // how many output data, from t_0 up to close to t_n
@@ -93,32 +100,35 @@ void InterpolateIMUData(const std::vector<Sophus::SE3Group<Scalar>, Eigen::align
         utprod<<0, 0, 2, 6*ut;
         ddotTildeBs=sixC*utprod/(6*Deltat*Deltat);
 
-        tripleA[0]=SE3Type::exp(Omegam22nm1[tickIndex]*tildeBs[1]);
-        tripleA[1]=SE3Type::exp(Omegam22nm1[tickIndex+1]*tildeBs[2]);
-        tripleA[2]=SE3Type::exp(Omegam22nm1[tickIndex+2]*tildeBs[3]);
-        dotDdotAs[0]=tripleA[0].matrix()*SE3Type::hat(Omegam22nm1[tickIndex])*dotTildeBs[1];
-        dotDdotAs[1]=tripleA[1].matrix()*SE3Type::hat(Omegam22nm1[tickIndex+1])*dotTildeBs[2];
-        dotDdotAs[2]=tripleA[2].matrix()*SE3Type::hat(Omegam22nm1[tickIndex+2])*dotTildeBs[3];
-        dotDdotAs[3]=tripleA[0].matrix()*SE3Type::hat(Omegam22nm1[tickIndex])*ddotTildeBs[1]+
-                dotDdotAs[0]*SE3Type::hat(Omegam22nm1[tickIndex])*dotTildeBs[1];
-        dotDdotAs[4]=tripleA[1].matrix()*SE3Type::hat(Omegam22nm1[tickIndex+1])*ddotTildeBs[2]+
-                dotDdotAs[1]*SE3Type::hat(Omegam22nm1[tickIndex+1])*dotTildeBs[2];
-        dotDdotAs[5]=tripleA[2].matrix()*SE3Type::hat(Omegam22nm1[tickIndex+2])*ddotTildeBs[3]+
-                dotDdotAs[2]*SE3Type::hat(Omegam22nm1[tickIndex+2])*dotTildeBs[3];
+        tripleA[0]=SE3Type::exp(Omega02np1[tickIndex]*tildeBs[1]);
+        tripleA[1]=SE3Type::exp(Omega02np1[tickIndex+1]*tildeBs[2]);
+        tripleA[2]=SE3Type::exp(Omega02np1[tickIndex+2]*tildeBs[3]);
+        dotDdotAs[0]=tripleA[0].matrix()*SE3Type::hat(Omega02np1[tickIndex])*dotTildeBs[1];
+        dotDdotAs[1]=tripleA[1].matrix()*SE3Type::hat(Omega02np1[tickIndex+1])*dotTildeBs[2];
+        dotDdotAs[2]=tripleA[2].matrix()*SE3Type::hat(Omega02np1[tickIndex+2])*dotTildeBs[3];
+        dotDdotAs[3]=tripleA[0].matrix()*SE3Type::hat(Omega02np1[tickIndex])*ddotTildeBs[1]+
+                dotDdotAs[0]*SE3Type::hat(Omega02np1[tickIndex])*dotTildeBs[1];
+        dotDdotAs[4]=tripleA[1].matrix()*SE3Type::hat(Omega02np1[tickIndex+1])*ddotTildeBs[2]+
+                dotDdotAs[1]*SE3Type::hat(Omega02np1[tickIndex+1])*dotTildeBs[2];
+        dotDdotAs[5]=tripleA[2].matrix()*SE3Type::hat(Omega02np1[tickIndex+2])*ddotTildeBs[3]+
+                dotDdotAs[2]*SE3Type::hat(Omega02np1[tickIndex+2])*dotTildeBs[3];
 
-        Ts2w=bm32nm1[tickIndex]*tripleA[0]*tripleA[1]*tripleA[2];
-        dotDdotTs[0]=bm32nm1[tickIndex].matrix()*dotDdotAs[0]*(tripleA[1]*tripleA[2]).matrix()+
-                (bm32nm1[tickIndex]*tripleA[0]).matrix()*dotDdotAs[1]*tripleA[2].matrix()+
-                (bm32nm1[tickIndex]*tripleA[0]*tripleA[1]).matrix()*dotDdotAs[2];
+        Ts2w=bm12np1[tickIndex]*tripleA[0]*tripleA[1]*tripleA[2];
+        dotDdotTs[0]=bm12np1[tickIndex].matrix()*dotDdotAs[0]*(tripleA[1]*tripleA[2]).matrix()+
+                (bm12np1[tickIndex]*tripleA[0]).matrix()*dotDdotAs[1]*tripleA[2].matrix()+
+                (bm12np1[tickIndex]*tripleA[0]*tripleA[1]).matrix()*dotDdotAs[2];
 
-        dotDdotTs[1]=bm32nm1[tickIndex].matrix()*dotDdotAs[3]*(tripleA[1]*tripleA[2]).matrix()+
-                (bm32nm1[tickIndex]*tripleA[0]).matrix()*dotDdotAs[4]*tripleA[2].matrix()+
-                (bm32nm1[tickIndex]*tripleA[0]*tripleA[1]).matrix()*dotDdotAs[5]+
-                2*bm32nm1[tickIndex].matrix()*(dotDdotAs[0]*dotDdotAs[1]*tripleA[2].matrix()+
+        dotDdotTs[1]=bm12np1[tickIndex].matrix()*dotDdotAs[3]*(tripleA[1]*tripleA[2]).matrix()+
+                (bm12np1[tickIndex]*tripleA[0]).matrix()*dotDdotAs[4]*tripleA[2].matrix()+
+                (bm12np1[tickIndex]*tripleA[0]*tripleA[1]).matrix()*dotDdotAs[5]+
+                2*bm12np1[tickIndex].matrix()*(dotDdotAs[0]*dotDdotAs[1]*tripleA[2].matrix()+
                 tripleA[0].matrix()*dotDdotAs[1]*dotDdotAs[2]+dotDdotAs[0]*tripleA[1].matrix()*dotDdotAs[2]);
 
         samplePoses[i]=Ts2w.matrix();
-        samples[i].segment(1,3)=dotDdotTs[1].col(3).head(3);//$a_s^w$
+        if (outputaccelinw)
+            samples[i].segment(1,3)=dotDdotTs[1].col(3).head(3);//$a_s^w$
+        else
+            samples[i].segment(1,3)=Ts2w.unit_quaternion().inverse()*(dotDdotTs[1].col(3).head(3) - gw);//$a_m^s$
         samples[i].segment(4,3)=SO3Type::vee(Ts2w.rotationMatrix().transpose()*dotDdotTs[0].topLeftCorner(3,3));//$\omega_{ws}^s$
         samples[i].tail(3)=dotDdotTs[0].col(3).head(3);//$v_s^w$
     }
