04eeb21dcab9c24c79f7e825c7ff71e719a22b15
jhuai <huai.3@osu.edu>
2022-11-16 21:01:43 +0800

    comments

--- ekfmonoslam/kinematics/rpy2rotm_ie.m
+++ ekfmonoslam/kinematics/rpy2rotm_ie.m
@@ -4,7 +4,11 @@ function ll_R_b = rpy2rotm_ie(rpy)
 % The body frame is in general front forward up.
 % The local level frame is east north up.
 % ll_R_b = R3(-yaw) * R1(-pitch) * R2(-roll). Note our R1(a) = IE R1(-a).
-% see eq 6 page 233 in Waypoint Software 8.90 User Manual v10 Aug 2020.
+% see page 50 bottom in waypoint software 6.0 user manual November 2014
+% and see eq 6 page 233 in Waypoint Software 8.90 User Manual v10 Aug 2020.
+% Note bynav uses the same notation as described here
+% https://www.bynav.com/cn/resource/bywork/healthy-work/654.html
+
 r = rpy(1);
 p = rpy(2);
 y = rpy(3);
--- ekfmonoslam/kinematics/test_euler_conversions.m
+++ ekfmonoslam/kinematics/test_euler_conversions.m
@@ -7,4 +7,11 @@ assert(norm(eul2 - eul3) < 3e-7);
 
 eul4 = Cbn2att(R);
 assert(norm(eul3 - eul4) < 3e-7);
+
+quat = quaternion(R, 'rotmat', 'point');
+eulZYX = quat2eul(quat, 'zyx'); % a row vector
+assert(norm(fliplr(eulZYX) - eul3') < 3e-7);
+
+Rp = quat2rotm(quat);
+assert(norm(Rp - R) < 3e-7);
 end
