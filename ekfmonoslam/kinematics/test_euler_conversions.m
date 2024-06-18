1c11131074314ee85fca5f3c2304f885e9c40f97
jhuai <huai.3@osu.edu>
2024-06-18 22:40:14 +0800

    retouch test euler

--- ekfmonoslam/kinematics/test_euler_conversions.m
+++ ekfmonoslam/kinematics/test_euler_conversions.m
@@ -1,6 +1,7 @@
 function test_euler_conversions()
 % Thankfully, ekfmonoslam, instk, oxford robotcar dataset devkit, MATLAB
 % are using the same Euler conventions.
+% Note matlab quat2dcm is exceptional.
 % As an aside, ROS tf transform, Eigen, scipy.spatial.transform.Rotation
 % also use the same Euler convention as can be seen from 
 % https://github.com/JzHuai0108/vio_common/blob/master/python/test/test_euler.py
@@ -25,5 +26,21 @@ eulZYX = quat2eul(quat, 'zyx'); % a row vector
 assert(norm(fliplr(eulZYX) - eul3') < 3e-7);
 
 Rp = quat2rotm(quat);
+Rpp = quat2rotm(compact(quat));
 assert(norm(Rp - R) < 3e-7);
+assert(norm(Rpp - R) < 3e-7);
+
+
+Ry = rotqr2ro(compact(quat)');
+assert(norm(Ry - R) < 3e-7);
+
+Rx = quat2dcm(quat);  % Note this is the transpose of R.
+assert(norm(Rx' - R) < 3e-7);
+
+qp = rotm2quat(R);
+assert(norm(qp - compact(quat)) < 3e-7);
+
+qz = dcm2quat(R);
+assert(norm(qz' - compact(quat)) < 3e-7);
+
 end
